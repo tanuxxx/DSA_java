a9d1a9be095a578b13edecda0ebc0ca481a14256
tanuxxx <tanmaymp92@gmail.com>
2025-07-30 17:39:05 +0530

    condition and looping

---
+++ src/B5_Fibonacci_series.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class B5_Fibonacci_series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter tearms: ");
+
+        int n = sc.nextInt();
+
+        int a = 0 , b = 1 ;
+
+
+        for(int i = 1; i<=n; i++){
+            System.out.print(a + " ");
+            int c = a + b;                 //changing the values of a,b,c;
+            a = b;
+            b = c;
+
+        }
+    }
+}
