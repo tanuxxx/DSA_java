923c02f5dc8b9b976f5ee3df0a178bbb8fd1c0eb
tanuxxx <tanmaymp92@gmail.com>
2025-07-30 12:14:15 +0530

    condition and looping

---
+++ src/B2_factorial_of_n.java
@@ -0,0 +1,12 @@
+public class B2_factorial_of_n{
+    public static void main(String[] args) {
+        int n = 4;
+        int fact =1;
+        int i = 1;
+        while (i<=n){
+            fact *= i;
+            i++;
+        }
+        System.out.println(fact);
+    }
+}
