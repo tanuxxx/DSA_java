1e708b88330cbeab748c42e68a6ba4a60490d8bf
tanuxxx <tanmaymp92@gmail.com>
2025-07-30 12:56:42 +0530

    condition and looping

---
+++ src/B4_count.java
@@ -0,0 +1,12 @@
+public class B4_count {
+    public static void main(String[] args) {
+        int count = 0;
+        long number = 2028;
+
+        while(number != 0){
+            number /= 10;        //divide the number by 10 upto the the number becomes 0.2028
+            count++;
+        }
+        System.out.println(count);
+    }
+}
