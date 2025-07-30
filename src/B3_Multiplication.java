c72da47cde6bcbab6f5cda67fb277e1e9f805c5b
tanuxxx <tanmaymp92@gmail.com>
2025-07-30 12:32:35 +0530

    condition and looping

---
+++ src/B3_Multiplication.java
@@ -0,0 +1,10 @@
+public class B3_Multiplication {
+    public static void main(String[] args) {
+        int n = 3 ;
+        int table = 1;
+        for(int i =1; i<=10 ; i++){
+            table = n * i;
+            System.out.println(table);
+        }
+    }
+}
