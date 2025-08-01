9f41b5b5f9b1627e0ea56ecc2373cc774e7e4319
tanuxxx <tanmaymp92@gmail.com>
2025-08-01 23:50:53 +0530

    power correction

--- src/power_6_A.java
+++ src/power_6_A.java
@@ -1,10 +1,14 @@
 public class power_6_A {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        int p ;
+        long a = 5;
+        long b = 3;
+        long p =1;
+        for(int i = 1; i<=b ; i++){
+            p *= a;
 
-        p = a ^ b ;
+        }
+
+
         System.out.println(p);
 
     }
