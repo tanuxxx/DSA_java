db791331cfd10693da879c490379cb4ee310b85c
tanuxxx <tanmaymp92@gmail.com>
2025-08-07 16:20:31 +0530

    first set is complete

--- src/A10_reverse_number.java
+++ src/A10_reverse_number.java
@@ -8,7 +8,6 @@ public class A10_reverse_number
         int num = sc.nextInt();
 
         int rev = 0;
-        int i = 1;
         while( num != 0){
             int digit = num % 10;         //get last digit
             rev = rev * 10 + digit;       //add digit to reversed number
